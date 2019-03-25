e8ac0ae27c597de3ffd5f68b271a56f0c6f92f12
Dgonzalez541 <36173383+Dgonzalez541@users.noreply.github.com>
2019-03-25 13:50:30 -0500

    Add files via upload
    Core functionality, reads paragraph from html then scans for language and creates mp3

---
+++ bs4test.py
@@ -0,0 +1,25 @@
+import nltk
+import os
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+from langdetect import detect
+from nltk.tokenize import sent_tokenize
+from gtts import gTTS
+from pydub import AudioSegment
+
+with open("mettest.html") as fp:
+    soup = BeautifulSoup(fp,"html.parser")
+
+tag_text = soup.p.get_text()
+sentence_list = sent_tokenize(tag_text)
+paragraph = AudioSegment.empty()
+
+for sentence in sentence_list:
+    lang = detect(sentence)
+    myobj = gTTS(text=sentence, lang=lang, slow=False)
+    myobj.save("sentence.mp3")
+    sentence = AudioSegment.from_mp3("sentence.mp3")
+    paragraph += sentence
+
+
+os.system("mpg321 paragraph.mp3")
