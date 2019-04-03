d07e52140e803757be031981ae7323048adddacb
Dgonzalez541 <36173383+Dgonzalez541@users.noreply.github.com>
2019-04-03 18:38:46 -0500

    Add files via upload
    Fixed epub issues with non html formats

--- webview3.py
+++ webview3.py
@@ -1,7 +1,10 @@
 import os
 import sys
 import ebooklib
+import shutil
+import zipfile
 from ebooklib import epub
+from ebooklib import utils
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
@@ -24,10 +27,13 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         # Initialize the Main Window
         super(MainWindow, self).__init__(parent)
+
         self.fileName = ''
         self.chapterList = []
         self.currentSection = 0
+
         self.create_menu()
+        self.createToolBar()
         self.add_web_widet()
         self.show()
 
@@ -61,14 +67,29 @@ class MainWindow(QMainWindow):
         choose_page_action.setShortcut('Ctrl+C')
         self.navigation_menu.addAction(choose_page_action)
 
-        #Toolbar
-        exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.triggered.connect(qApp.quit)
+    def createToolBar(self):
 
+        #Toolbar
+        exitAct = QAction(QIcon.fromTheme('exit'), 'Exit', self)
+        #exitAct.setShortcut('Ctrl+Q')
+        exitAct.triggered.connect(qApp.quit)
         self.toolbar = self.addToolBar('Exit')
         self.toolbar.addAction(exitAct)
 
+        prevPageAct = QAction(QIcon.fromTheme('go-previous'),'Prev',self)
+        #prevPageAct.setShortcut('Ctrl+P')
+        prevPageAct.triggered.connect(self.pageTurnPrev)
+        self.toolbar.addAction(prevPageAct)
+
+        nextPageAct = QAction(QIcon.fromTheme('go-next'),'Next',self)
+        #nextPageAct.setShortcut('Ctrl+N')
+        nextPageAct.triggered.connect(self.pageTurnNext)
+        self.toolbar.addAction(nextPageAct)
+
+        #Combobox for toolbar
+        self.combo=QComboBox()
+        self.toolbar.addWidget(self.combo)
+        self.combo.addItems(self.chapterList)
 
 
     #File dialog box
@@ -111,20 +132,42 @@ class MainWindow(QMainWindow):
 
     #Epub Handler
     def epubHandler(self):
-        print("epub handler called")
-        book = epub.read_epub(self.fileName)
 
-        for chapter in book.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+        book = self.fileName
+        zip = 'temp.zip'
+        directory = 'tempDir'
+
+        shutil.copyfile(book, zip)
+
+        zip_ref = zipfile.ZipFile(zip, 'r')
+        zip_ref.extractall(directory)
+        zip_ref.close()
+
+        bookRead = epub.read_epub(self.fileName)
+
+        for chapter in bookRead.get_items_of_type(ebooklib.ITEM_DOCUMENT):
+
+            #parse file path to content file from item object inside OBES folder
             chapter = str(chapter)
             first, second, third = chapter.split(':')
             third = third[:-1]
-            file = self.fileName
-            file = file[:-5]
-            href = file + '/' + third
-            self.chapterList.append(href)
 
-        print(self.chapterList[0])
+            if (third.endswith('.htm') or third.endswith('.xml') or third.endswith('.xhtml')):
+                name, extension = third.split('.')
+                src = os.getcwd() + "/" + directory + "/" +  third
+                dest = os.getcwd() + "/" + directory + "/" + name + ".html"
+                print("Adjusted dest" + dest)
+                os.rename(src,dest)
+                self.chapterList.append(dest)
+            else:
+                file = self.fileName
+                file = file[:-5]
+                href = file + '/' + third
+                self.chapterList.append(href)
+
         self.web_widget.load(QUrl.fromLocalFile(self.chapterList[0]))
+        self.combo.addItems(self.chapterList)
+
 
     #Adds web widget to central widget
     def add_web_widet(self):
@@ -144,6 +187,7 @@ class MainWindow(QMainWindow):
         if((self.currentSection - 1) >= 0):
             self.web_widget.load(QUrl.fromLocalFile(self.chapterList[self.currentSection - 1]))
             self.currentSection = self.currentSection - 1
+
 
 
 if __name__ == "__main__":
